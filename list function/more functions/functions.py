2b3a150855e20478fc0951ad94d10aa91f2ea395
amul patle <amulpatle3@gmail.com>
2023-09-06 17:10:27 +0530

    zip fucntion

---
+++ list function/more functions/functions.py
@@ -0,0 +1,15 @@
+l = [10,45,67,33,45,11,22,11,45,10,91]
+
+print(l.count(45))
+
+print(max(l))
+
+print(min(l))
+
+l.reverse()
+print(l)
+
+l.sort()
+print(l)
+
+print(l.index(45))
---
+++ list function/zip function /zip.py
@@ -0,0 +1,8 @@
+l = [10,23,76,89,0,3]
+li = [98,54,33,23,1,6]
+
+for a,b in zip(l,li):
+    print(a,b)
+
+# for i in range(len(l)):
+#     print(l[i],li[i])
