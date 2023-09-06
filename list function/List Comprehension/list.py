bb8c168e63ce572e4bd862d1ebe29daf6dbb5a34
amul patle <amulpatle3@gmail.com>
2023-09-06 16:28:30 +0530

    List Comprehension

---
+++ list function/List Comprehension/list.py
@@ -0,0 +1,16 @@
+l= []
+
+for a in range(1,101):
+    l.append(a)   
+
+print(l)
+
+print("   ")
+li = [i for i in range(1,101) if i%2 == 0]
+
+print(li)
+
+print("  ")
+str = "hii! my name is Amul"
+lis = [i for i in str]
+print(lis)
