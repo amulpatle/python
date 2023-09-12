46a09a4d619896f32d306b5e53db73866af04c07
amul patle <amulpatle3@gmail.com>
2023-09-13 01:31:22 +0530

    tupple

---
+++ Tuple/tuple.py
@@ -0,0 +1,19 @@
+t = (10,20,30,40,50)
+
+for i in t :
+    print(i)
+print(" ")
+for i in range(len(t)):
+    print(t[i])
+
+print(" ")
+print(min(t))
+
+print(" ")
+print(max(t))
+
+print(" ")
+print(t.index(40))
+
+print(" ")
+print(sum(t,10))
